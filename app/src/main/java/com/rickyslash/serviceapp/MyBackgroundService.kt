137b3723db01da009e0f40fa03a6d869991b6acf
ricky-kiva <ricky.kiva@gmail.com>
2023-04-25 00:27:08 +0700

    add service stop functionality

--- app/src/main/java/com/rickyslash/serviceapp/MyBackgroundService.kt
+++ app/src/main/java/com/rickyslash/serviceapp/MyBackgroundService.kt
@@ -4,12 +4,13 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.*
 
 class MyBackgroundService : Service() {
 
-    companion object {
-        internal val TAG = MyBackgroundService::class.java.simpleName
-    }
+    private val serviceJob = Job()
+    // this means it runs on main thread, but the lifecycle is tied to this Service
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
     override fun onBind(intent: Intent): IBinder {
         throw UnsupportedOperationException("Not yet implemented")
@@ -18,7 +19,26 @@ class MyBackgroundService : Service() {
     // this will triggered when this service is started
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG, "My service is running!")
+        serviceScope.launch {
+            for (i in 1..20) {
+                delay(1000)
+                Log.d(TAG, "Do something $i")
+            }
+            stopSelf()
+            Log.d(TAG, "Service is stopped!")
+        }
         // `START_STICKY` will make the system to restart the service if it's killed, but won't redeliver original Intent
         return START_STICKY
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceJob.cancel()
+        Log.d(TAG, "Service disbanded.")
+    }
+
+    companion object {
+        internal val TAG = MyBackgroundService::class.java.simpleName
+    }
+
 }
